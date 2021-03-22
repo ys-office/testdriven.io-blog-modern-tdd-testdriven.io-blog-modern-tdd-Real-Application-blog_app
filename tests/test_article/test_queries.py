85e2ad981e47d4dc11121323a6a264d608baf43f
Yusuke Sato <yusuke.sato@ys-office.me>
2021-03-22 21:52:02 +0900

    Add a new test for GetArticleByIDQuery to test_queries.py.:

--- tests/test_article/test_queries.py
+++ tests/test_article/test_queries.py
@@ -22,3 +22,22 @@ def test_list_articles():
     query = ListArticlesQuery()
 
     assert len(query.execute()) == 2
+
+
+def test_get_article_by_id():
+    """
+    GIVEN ID of article stored in the database
+    WHEN the execute method is called on GetArticleByIDQuery with id set
+    THEN it should return the article with the same id
+    """
+    article = Article(
+        author='jane@doe.com',
+        title='New Article',
+        content='Super extra awesome article'
+    ).save()
+
+    query = GetArticleByIDQuery(
+        id=article.id
+    )
+
+    assert query.execute().id == article.id
